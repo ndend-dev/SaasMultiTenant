cdac5d981b3509ff7700745fc9528ae043895ec5
ndend <ndend2009@hotmail.com>
2026-05-20 12:52:42 -0500

    first commit

---
+++ Database/script.sql
@@ -0,0 +1,10 @@
+/*
+-------------------------------------------------------------------------------
+Author:       Yeisson Duvan Rodriguez Herrera
+Create Date:  2026-05-20
+Description:  Script Saas MUlti Tenant.
+              
+Modified By:  [Nombre] - [Fecha] - [Motivo del cambio]
+-------------------------------------------------------------------------------
+*/
+
